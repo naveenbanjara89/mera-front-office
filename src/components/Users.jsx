c295bdd398ea74557873112d1b127d13b1ccd3c4
Naveen Banjara <161052612+naveenbanjara89@users.noreply.github.com>
2024-10-06 17:54:04 +0530

    Update Users.jsx

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -15,7 +15,7 @@ const Users = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/users");
+        const response = await fetch("https://backendoffice.vercel.app/users");
         const data = await response.json();
         setRows(data);
       } catch (error) {
