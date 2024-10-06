bd7207e8e190cce3e6a3766029569d28028348fd
Naveen Banjara <161052612+naveenbanjara89@users.noreply.github.com>
2024-10-06 18:20:58 +0530

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -19,7 +19,7 @@ const Tasks = () => {
       setLoading(true);
       try {
         const response = await axios.get(
-          "https://backendoffice.vercel.app/tasklists"
+          "https://mera-office.onrender.com/tasklists"
         );
         const taskData = [];
 
